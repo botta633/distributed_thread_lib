dfffbedd4dc9cf94d0dc83f6f6048310ad4becb3
botta633 <bottaawesome633@gmail.com>
2025-01-08 02:37:00 +0200

    support passing arguments and signal masking

--- thread_create.c
+++ thread_create.c
@@ -1,5 +1,6 @@
 #include "list.h"
 #include "thread_internal.h"
+#include <signal.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdint.h>
@@ -9,38 +10,66 @@
 #include <sys/syscall.h>    /* Definition of SYS_* constants */
 #include <unistd.h>
 #include <sys/wait.h>
+#include <err.h>
 #define PAGE_SIZE 4096
 #define STACK_SIZE (2 * 1024 * 1024)
 #define INITIAL_PRIO 5
+
+typedef void *(*FuncPointer)(void *);
+typedef void (*voidFuncPointer)(void *);
+
+typedef struct {
+    FuncPointer start_routine;
+    void *args;
+} thread_args;
 
 void init_threadLib() {
 
     thread_list = malloc(sizeof(dllist_t));
     thread_list->head = NULL;
     thread_list->offset = offsetof(thread_t, node);
-
 }
 
-typedef void *(*FuncPointer)(void *);
+static void thread_start(void *arg)  {
+    // Extract thread function and argument from the passed struct
+    // Unblock signals in the child thread
+    sigset_t mask;
+    sigemptyset(&mask);
+    sigprocmask(SIG_SETMASK, &mask, NULL);
+
+    thread_args *args = *(thread_args **)((uintptr_t)__builtin_frame_address(0) + 8);
+    args->start_routine(args->args);  // Execute the start routine
+
+    free(args);
+    _exit(0);
+}
+
 int thread_create(thread_t *new_thread, FuncPointer start_routine, void *arg)
 {
     void *stackaddr;
     void *stacktop;
+    sigset_t new_mask, old_mask;
+    thread_args *args = malloc(sizeof(thread_args));
+    printf("address of real args is %p\n", args);
 
     stackaddr = mmap(NULL, STACK_SIZE, PROT_READ|PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS | MAP_STACK, -1, 0);
     if(!stackaddr)
         return -1;
 
+    args->start_routine = start_routine;
+    args->args = arg;
+
     stacktop = stackaddr + STACK_SIZE;
-    stacktop = (void *)((long)stacktop & ~0xF);
-    long func_location = (long)stacktop - 8;
+    long func_location = (long)stacktop - 16;
+    long args_location = func_location + 8;
+    *((voidFuncPointer *)(func_location)) = thread_start;
+    *((thread_args **)(args_location)) = args;
+
     new_thread->stackaddr = (void *)func_location;
-    *((FuncPointer *)func_location) = *start_routine;
     new_thread->stacksize = STACK_SIZE;
     new_thread->start_routine = start_routine;
     add_node(thread_list, &(new_thread->node));
 
-    
     long flags = 0;
     flags |= CLONE_VM;
     flags |= CLONE_FS;
@@ -50,47 +79,43 @@ int thread_create(thread_t *new_thread, FuncPointer start_routine, void *arg)
     flags |= CLONE_PARENT;
     flags |= CLONE_IO;
 
-    long res = syscall(SYS_clone, flags, (void *)func_location);
-    printf("%ld\n", res);
-   
-  
-/*
-    long res;
-   __asm__ (
+    // Block all signals before thread creation
+    sigfillset(&new_mask);  // Block all signals
+    sigprocmask(SIG_BLOCK, &new_mask, &old_mask);  // Save old mask
 
-        "mov rax, 56            \n\t"  
-        "mov rdi, %0            \n\t"  
-        "lea rsi, %1            \n\t"
-        "mov [rsi], %2"
-        "syscall                \n\t"
-        "ret \n\t"
-        : "=r" (res)        // Output operand: store the return value (rax) in child_pid
-        : "r" (flags),      // Input: flags (e.g., CLONE_VM)
-          "m" (func_location), // Input: child stack address (must be the top of the stack)
-          "r" (start_routine)
-        : "%rax", "%rdi", "%rsi", "%rdx"  // Clobbered registers
-    );
-    */
+    long pid = syscall(SYS_clone, flags, (void *)func_location);
+    if (pid == -1) {
+        perror("clone failed");
+        munmap(stackaddr, STACK_SIZE);
+        return -1;
+    }
 
-     return 0;
+    sleep(3);
+    if (waitpid(pid, NULL, 0) == -1) {
+        perror("waitpid failed");
+        return -1;
+    }
+    // Restore the original signal mask in the parent process
+    sigprocmask(SIG_SETMASK, &old_mask, NULL);
+    printf("child has terminated\n");
+
+    return 0;
 }
 
-
 /*
-void *test_func(void * args) {
-    printf("Came here hopefully by another thread\n");
-    exit(0);
+void *test_func(void *args) {
+    printf("This is %s\n",(char*)args);
+    return 0;
 }
 
 int main() {
     init_threadLib();
     thread_t *new_thread = malloc(sizeof(thread_t));
 
-    thread_create(new_thread, &test_func, NULL);
-    
+    thread_create(new_thread, &test_func, "Hello from thread");
+
     return 0;
 }
+
 */
-
-
 
